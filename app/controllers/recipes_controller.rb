0a9c7b9b3b4733a9f4d52c0f3385d646eeb5ea7f
TylerMcGowan <komotopic@gmail.com>
2021-06-22 17:55:08 -0400

    Added edit

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,16 +5,6 @@ class RecipeController < ApplicationController
         erb :'recipes/recipe_index'
     end
 
-    post '/recipes' do
-        if !is_logged_in?
-            redirect to '/login'
-        end
-        recipe = Recipe.new(params)
-        recipe.save
-        binding.pry
-        redirect to '/recipes'
-    end
-
     get '/recipes/new' do
         if !is_logged_in?
             redirect to '/login'
@@ -24,26 +14,51 @@ class RecipeController < ApplicationController
     end
 
     
+    
 
     get '/recipes/:id' do
         if !is_logged_in?
             redirect to '/login'
         end
         @recipe = Recipe.find(params[:id])
-        binding.pry
+        
 
         erb :'recipes/show'
     end
 
-    get 'recipes/:id/edit' do
+    get '/recipes/:id/edit' do
+        if !is_logged_in?
+            redirect to '/login'
+        else
+            @recipe = Recipe.find(params[:id])
+            if @recipes.user != current_user
+                redirect to '/recipes'
+            end
+            erb :'recipes/edit'
+        end
+    end
+
+    post '/recipes' do
         if !is_logged_in?
             redirect to '/login'
         end
+        @recipe = Recipe.new(params)
+        @recipe.user_id = session[:user_id]
+        @recipe.save
+        
+        redirect to '/recipes'
     end
 
     patch '/recipes/:id' do
         if !is_logged_in?
             redirect to '/login'
+        else @recipe = Recipe.find(params[:id])
+            if @recipes.user != current_user
+                redirect to '/recipes'
+            else
+                @recipe.update(params["recipe"])
+                redirect :'recipe/#{@recipe.id}'
+            end
         end
     end
 
